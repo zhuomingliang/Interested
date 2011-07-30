45c2aa5239a91165e5078a28f4308812b3e3a8d0
Jimmy Zhuo <zhuomingliang@yahoo.com.cn>
2011-07-30 18:28:48 +0800

    add some code

--- focalprice.pl
+++ focalprice.pl
@@ -1,8 +1,10 @@
 #! /usr/bin/env perl
 use strict;
 use warnings;
+use utf8;
 
 use WWW::Mechanize;
+use DBI;
 
 sub get_category_urls {
 	my $mech = WWW::Mechanize->new();
@@ -75,9 +77,13 @@ sub get_product_urls {
 }
 
 sub get_product {
+	my $dbh = DBI->connect("DBI:mysql:database=forcalbuying;host=localhost", "root", "12345", {'RaiseError' => 1}) or die $DBI::errstr;;
+
 	open(OUTFILE, "< products.txt");
+
 	my $line = 1;
 	my @failed_lists;
+
 	while(<OUTFILE>) {
 		my $mech = WWW::Mechanize->new();
 		print "getting Line $line ", $_, "...";
@@ -91,13 +97,36 @@ sub get_product {
 			print "failed\n";
 			push @failed_lists, $_;
                 } elsif ($mech->success() && $mech->status() == 200) {
-			print "success\n";
-			my ($price) = $mech->content() =~ /Priceus">\s*\$([\d\.]+)/;
-			print $price, "\n";
+			my $content = $mech->content();
+			if ($content =~ /tocart12/) { # out of stock
+				print "skipped\n";
+				next;
+			}
 
+			print "success\n";
+			my ($title) = $content =~ /<title>([^<]+)<\/title>/s;
+print $title, "\n";
+			my ($price) = $content =~ /Priceus">\s*\$([\d\.]+)/s;
+			print $price, "\n";
+			my (@category) = $content =~ /[^>]+>([^<]+)<\s*\/a\s*>\s*>\s*/sg;
+print join ' > ', @category;
+print "\n";
+			my (@pics) = $content =~ /registerImage\("alt_image_\d+", "([^"]+)/sg;
+
+print join "\n", @pics;
+print "\n";
+			my ($desc) = $content =~ /class="goods_text">(.*)<\/div>\s*<div\s+style="padding:10px;">/s;
+
+print $desc;
+			my ($desc_more) = $content =~ /<div\s+style="padding:10px;">(.*)<\/div>\s*<\/div>\s*<div\s+class="TabContent"/s;
+			$desc_more =~ s/×/x/sg;
+print $desc_more;
 			#@products =  $mech->content() =~ /<ul\s+class\="infoBox">\s*<li\s+class\="proImg">\s*<a\s+href\="([^"]+)">/g;
 			#print OUTFILE join "\n", @products;
 			#print OUTFILE "\n";
+			my (@wholesale_price) = $content =~ /<td>\s*<font\s+color='#ff6600'>\s*\$\s*([^<]+)/sg;
+print join ' ', @wholesale_price;
+exit;
 		} else {
 			print "failed\n";
 			push @failed_lists, $_;
